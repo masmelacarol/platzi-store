f290d4a48010221bfab28c735784223d149c33ee
masmelacarol <carolstefannym@gmail.com>
2020-07-22 11:29:46 -0500

    shopping cart icon

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,21 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { CartService } from 'src/app/core/services/cart/cart.service';
-
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
-  total = 0;
+  total$: Observable<number>;
   constructor(private cartService: CartService) {
-    this.cartService.cart$.subscribe((cartProducts) => {
-      console.log(
-        'HeaderComponent -> constructor -> cartProducts',
-        cartProducts
-      );
-      this.total = cartProducts.length;
-    });
+    this.total$ = this.cartService.cart$.pipe(
+      map((cartProducts) => cartProducts.length)
+    );
   }
 
   ngOnInit(): void {}
