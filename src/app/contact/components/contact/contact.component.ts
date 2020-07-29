a9c454bbe4c73d08eed342955f8dd06b2d7d32f5
masmelacarol <carolstefannym@gmail.com>
2020-07-29 18:31:43 -0500

    about

--- src/app/contact/components/contact/contact.component.ts
+++ src/app/contact/components/contact/contact.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.scss']
+  styleUrls: ['./contact.component.scss'],
 })
 export class ContactComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  form: FormGroup;
+  constructor(private formBuilder: FormBuilder) {
+    this.buildForm();
   }
 
+  ngOnInit(): void {}
+
+  private buildForm(): void {
+    this.form = this.formBuilder.group({
+      name: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+      message: ['', [Validators.required, Validators.minLength(40)]],
+    });
+  }
+
+  sendEmail(event: Event) {
+    event.preventDefault();
+    if (this.form.valid) {
+      console.log('hola');
+    } else {
+      console.log('error');
+    }
+  }
 }
--- src/app/contact/contact.module.ts
+++ src/app/contact/contact.module.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { MaterialModule } from '@material/material.module';
 import { SharedModule } from '@shared/shared.module';
 import { ContactComponent } from './components/contact/contact.component';
 import { ContactRoutingModule } from './contact.routing.module';
 
 @NgModule({
   declarations: [ContactComponent],
-  imports: [CommonModule, SharedModule, ContactRoutingModule],
+  imports: [CommonModule, SharedModule, ContactRoutingModule, MaterialModule],
 })
 export class ContactModule {}
